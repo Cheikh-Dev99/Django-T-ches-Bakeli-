46bdeffbac5bf940bb37e013168dfeaa79fd1107
CheikhDev <cheikhdev99@gmail.com>
2024-10-01 16:05:59 +0000

    Commit du 01/10/2024 : Balise et Filtres en Django 1

--- Introduction_django/mangalib/views.py
+++ Introduction_django/mangalib/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+import datetime
+import random
+from utils.citations import citations
 
 def index(request):
-  contexte = {'titre': 'Bienvenue sur Mangalib',
-              'message': 'MangaLib est un site web destiner aux amoureux du Mangas'
-  }
-  return render(request, 'mangalib/index.html', contexte)
+    D1 = datetime.datetime.now()
+    D2 = D1 - datetime.timedelta(minutes=random.randint(1, 60))  # D2 est 30 minutes avant D1
+    mesCitations = citations
+    contexte = {
+        'titre': 'Bienvenue sur Mangalib',
+        'titre2': 'Django : Balises & Filtres',
+        'message': "Django est un framework de développement web qui facilite la  création d'applications web en Python.",
+        'message1': 'Hello World !',
+        "D1": D1,
+        "D2": D2,
+        'citations': mesCitations,
+        'username': "Cheikh",
+        'numberOfNews': random.randint(0, 100),
+        'link': "http://mesfilmsetseries.sn/index.html?categorie=action&p=2",
+        'text': "Vous pouvez retrouver plus de films et de series sur http://Film&Serie.sn"
+    }
+    return render(request, 'mangalib/index.html', contexte)
